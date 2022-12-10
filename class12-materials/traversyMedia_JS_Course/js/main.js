424c007a86ed4faa2ba2332d1624b72d809019a8
owaisbukhari00 <owaisbukhari@outlook.com>
2022-12-10 20:07:54 +0500

    [JS] strings

--- class12-materials/traversyMedia_JS_Course/js/main.js
+++ class12-materials/traversyMedia_JS_Course/js/main.js
@@ -3,7 +3,7 @@
 //var -> global scope , not recommended
 //let -> can be reassigned
 //const -> cannot be reassigned
-
+/*
 // example
 const age1 = 30;
 console.log(age1);
@@ -11,5 +11,22 @@ console.log(age1);
 let age2 = 30;
 age2 = 34;
 console.log(age2);
+*/
 
+//String, numbers, boolean, null, undefined, symbol
 
+const name1 = 'john';
+const age = 30;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z; // z= undefined on default
+
+console.log(name1);
+console.log(typeof(name1));
+console.log(typeof(rating));
+console.log(typeof(isCool));
+console.log(typeof(x));
+console.log(typeof(y));
+console.log(typeof(z));
