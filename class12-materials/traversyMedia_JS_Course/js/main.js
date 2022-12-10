844b937ffc304f77ddf5d74813ca2b784383f2a6
owaisbukhari00 <owaisbukhari@outlook.com>
2022-12-10 20:07:04 +0500

    traversyMedia JS crash course template

---
+++ class12-materials/traversyMedia_JS_Course/js/main.js
@@ -0,0 +1,15 @@
+/* variables */
+
+//var -> global scope , not recommended
+//let -> can be reassigned
+//const -> cannot be reassigned
+
+// example
+const age1 = 30;
+console.log(age1);
+
+let age2 = 30;
+age2 = 34;
+console.log(age2);
+
+
