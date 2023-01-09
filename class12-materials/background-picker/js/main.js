82e8f6514810740a7e6620b100ec5a6bb4711ab9
owaisbukhari00 <owaisbukhari@outlook.com>
2023-01-10 00:04:07 +0500

    modified and added a newcase

--- class12-materials/background-picker/js/main.js
+++ class12-materials/background-picker/js/main.js
@@ -1,6 +1,7 @@
 document.getElementById('purple').onclick = partyPurple
 document.getElementById('green').onclick = partyGreen
 document.getElementById('blue').onclick = partyBlue
+document.getElementById('red').onclick = partyRed
 
 
 function partyPurple() {
@@ -16,4 +17,9 @@ function partyGreen() {
 function partyBlue() {
   document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
   document.querySelector('body').style.color = 'white'
+}
+
+function partyRed() {
+  document.querySelector("body").style.backgroundColor = 'rgba(200, 0, 0, 1)';
+  document.querySelector("body").style.color = 'white';
 }
