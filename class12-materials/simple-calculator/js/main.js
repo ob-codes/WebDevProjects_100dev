96c67496752953fbc7eaa7862b9807926fc8f300
owaisbukhari00 <owaisbukhari@outlook.com>
2023-01-21 00:12:00 +0500

    added one case

--- class12-materials/simple-calculator/js/main.js
+++ class12-materials/simple-calculator/js/main.js
@@ -4,6 +4,7 @@ document.getElementById("pumpkin").addEventListener('click', makeZero);
 document.querySelector("#dominosPizza").addEventListener('click', addThree);
 document.querySelector("#zebra").addEventListener('click', addNine);
 document.getElementById("cantThinkOfAnything").addEventListener('click', subTwo);
+document.getElementById("multiple").addEventListener("click", multiplyTen);
 
 
 function makeZero(){
@@ -24,4 +25,9 @@ function addNine(){
 function subTwo(){
   total -= 2;
   document.querySelector("#placeToPutResult").innerHTML = total;
+}
+
+function multiplyTen(){
+  total *= 10;
+  document.getElementById("placeToPutResult").innerText = total;
 }
