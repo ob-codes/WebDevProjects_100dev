b7d1f2319723ec3b12ad5c8492d754e7f6c9f6e5
owaisbukhari00 <owaisbukhari@outlook.com>
2023-01-23 23:45:27 +0500

    functions practice

---
+++ class13-materials/simple-functions/js/main.js
@@ -0,0 +1,20 @@
+//create a function that adds two numbers and alerts the sum
+function addTwo(num1, num2)
+{
+  alert(num1 + num2);
+}
+addTwo(12, 2);
+//create a function that multiplys three numbers and console logs the product
+function multiplyThree(num1, num2, num3)
+{
+  product = num1*num2*num3;
+  console.log(product);
+}
+multiplyThree(2, 3, 4);
+//create a function that divides two numbers and returns the ???
+function divideTwo(num1, num2)
+{
+  division = num1 / num2;
+  return division;
+}
+console.log(divideTwo(12, 4));
