79f6d0336959139a62df445fb291dbf827e7b31a
owaisbukhari00 <owaisbukhari@outlook.com>
2023-01-24 22:51:23 +0500

    some class examples

---
+++ class14-materials/simple-functions/js/main.js
@@ -0,0 +1,15 @@
+//create a function that adds two numbers and alerts the sum
+function addNums(num1, num2){
+  alert(num1+num2);
+}
+addNums(3, 4);
+//create a function that multiplys three numbers and console logs the product
+function multiply(num1, num2, num3){
+  console.log(num1* num2* num3);
+}
+multiply(3, 4, 2);
+//create a function that divides two numbers and returns the ???
+function divide(num1, num2){
+  return (num1 / num2);
+}
+console.log(divide(7,2));
