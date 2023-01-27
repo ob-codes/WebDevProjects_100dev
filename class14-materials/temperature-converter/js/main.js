d3e22d49b1b01567048dee0637cf79443c703603
owaisbukhari00 <owaisbukhari@outlook.com>
2023-01-27 20:16:50 +0500

    added pseudo-code

--- class14-materials/temperature-converter/js/main.js
+++ class14-materials/temperature-converter/js/main.js
@@ -1,4 +1,8 @@
 //Write your pseduo code first! 
+//1.take temp input in celsius
+//2.calculate fareinheight
+//3.display result
+
 let temp = 0;
 
 document.querySelector("#run").addEventListener("click", calculation)
