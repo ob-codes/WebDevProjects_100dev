9056470d2ccfaf5da2d62e33f2de0e6a276d0f34
owaisbukhari00 <owaisbukhari@outlook.com>
2023-01-30 21:25:09 +0500

    practice exercise

---
+++ class14-materials/bachelor-code/1/js/main.js
@@ -0,0 +1,7 @@
+
+document.querySelector("#finalRose").addEventListener("click", check);
+
+function check(){
+	document.querySelector("#claire").style.display = "none";
+	document.querySelector("#sharleen").style.display = "none";
+}
