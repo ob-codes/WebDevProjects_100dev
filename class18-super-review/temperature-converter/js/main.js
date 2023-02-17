08631dffd9e3798b52b55b37a1f8f222b80b3cbe
owaisbukhari00 <owaisbukhari@outlook.com>
2023-02-18 00:04:33 +0500

    algo practice

---
+++ class18-super-review/temperature-converter/js/main.js
@@ -0,0 +1,14 @@
+//Write your pseduo code first! 
+//take input 
+document.querySelector("h1").addEventListener("click", run)
+
+function run() {
+  let num;
+  num = Number(document.querySelector("input").value);
+
+  //perform arithmetic
+  let result = num * 9/5 +32;
+  
+  //spell out the result
+  document.querySelector("#placeForResult").innerHTML = result;
+}
