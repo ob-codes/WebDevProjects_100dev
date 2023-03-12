1ce4009a1d9dc6df4f77ff6d99d6355c3a4af55d
owaisbukhari00 <owaisbukhari@outlook.com>
2023-03-12 22:07:29 +0500

    review and practice

---
+++ class20-materials/review-in-paradise/js/main.js
@@ -0,0 +1,46 @@
+// *Variables*
+// Declare a variable, reassign it to your favorite food, and alert the value
+let food1;
+food1="biryani";
+
+alert(food1);
+//Declare a variable, assign it a string, alert the second character in the string (Use your google-fu and the MDN)
+let str1;
+str1="its a string";
+
+alert(str1[1]);
+
+// *Functions*
+// Create a function that takes in 3 numbers. Divide the first two numbers and multiply the last. Alert the product. Call the function.
+function maths(n1,n2,n3){
+  alert((n1/n2) * n3);
+}
+
+maths(12,3,4);
+// Create a function that takes in 1 number. Console log the cube root of the number. Call the function.
+function cubeRoot(n1){
+  console.log(n1*n1*n1);
+}
+cubeRoot(2);
+// *Conditionals*
+//Create a function that takes in a month. If it is a summer month alert "YAY". If another other month, alert "Booo"
+function yayOrBoo(mon){
+  if (mon > 3 && mon < 9)
+    alert("YAY");
+  else
+    alert("Boo");
+}
+
+yayOrBoo(4);
+
+//*Loops*
+//Create a function that takes in a number. Console log every number from 1 to that number while skipping multiples of 5.
+function skip5(n1){
+  for (let i=1;i<=n1;i++){
+    if(i%5 === 0) 
+      continue;
+    else
+      console.log(i);
+  }
+}
+skip5(27);
