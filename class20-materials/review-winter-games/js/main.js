59583aee0bfcac6bf88dda76ca5566532fbad2b2
owaisbukhari00 <owaisbukhari@outlook.com>
2023-03-14 00:39:27 +0500

    ammended the condition

--- class20-materials/review-winter-games/js/main.js
+++ class20-materials/review-winter-games/js/main.js
@@ -1,10 +1,25 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
-function takeNums(arr1){
+function onlyEvens(arr1){
   let arr2=[];
+  let idx=0;
 
-  arr2 = arr1;
-
+  /* Approach-1 */
+  /*
+  for(let i=0;i<arr1.length;i++){
+    if (arr1[i] %2 ==0)
+      arr2[idx++] = arr1[i];
+  }
+  */
+  /* Approach-2 */
+  
+  arr2 = arr1.map(x => {
+    if(x % 2== 0)
+      return x;
+    else
+      return "";
+  })
+  
   return arr2;
 }
 
-console.log(takeNums([1,2,3,4]));
+console.log(onlyEvens([1,2,3,4]));
