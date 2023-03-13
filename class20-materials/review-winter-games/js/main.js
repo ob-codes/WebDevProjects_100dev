6212ec4c0091e35cc345f1a26d2dce3371efdd22
owaisbukhari00 <owaisbukhari@outlook.com>
2023-03-14 00:13:06 +0500

    arrays

---
+++ class20-materials/review-winter-games/js/main.js
@@ -0,0 +1,10 @@
+//Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+function takeNums(arr1){
+  let arr2=[];
+
+  arr2 = arr1;
+
+  return arr2;
+}
+
+console.log(takeNums([1,2,3,4]));
