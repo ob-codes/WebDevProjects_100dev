a8b2aca00f0e8f65246ae37cbf22160128d09074
owaisbukhari00 <owaisbukhari@outlook.com>
2023-03-17 22:52:54 +0500

    char code comparison method

---
+++ CodeWars/Regex validate PIN code/main.js
@@ -0,0 +1,16 @@
+function validatePIN (pin) {
+  //return true or false
+  pin = pin.toLowerCase();
+
+  if (pin.length !== 4 && pin.length !== 6)
+    return false;  
+  
+  for (let i = 0; i < pin.length; i++) {
+    if (pin.charCodeAt(i) < 48 || pin.charCodeAt(i) > 57)    
+      return false;
+  }    
+   
+  return true;
+}
+
+console.log(validatePIN("123x"));
