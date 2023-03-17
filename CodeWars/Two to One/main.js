472cc9a82a8256dbf3fcd4e9fa95f2111fb787ff
owaisbukhari00 <owaisbukhari@outlook.com>
2023-03-17 22:00:24 +0500

    distinct characters from array

--- CodeWars/Two to One/main.js
+++ CodeWars/Two to One/main.js
@@ -1,12 +1,14 @@
 function longest(s1, s2) {
-  let str = "";  
-  str = s1 + s2;
-  str = str.toLowerCase();
-  str = str.split("");
+  let str = s1 + s2;
+  str = str.toLowerCase().split("");
   
-  str.sort();
-  str = str.join('');
-  return str;
+  let result ="";
+  for (let i = 97; i<=122; i++) {
+    if(str.includes(String.fromCharCode(i))){
+      result += String.fromCharCode(i);
+    }
+  } 
+  return result;
 }
 
 console.log(longest("areTHeyhere", "yestheyarehere"));
