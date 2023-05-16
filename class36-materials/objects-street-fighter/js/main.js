a06a6e5cadd79ce800f79e25f18c27de04969d91
owaisbukhari00 <owaisbukhari@outlook.com>
2023-05-17 00:20:48 +0500

    JS super review constructor

---
+++ class36-materials/objects-street-fighter/js/main.js
@@ -0,0 +1,22 @@
+//Create a street fighter constructor that makes fighting game 
+//characters with 4 properties and 3 methods
+
+function MakeFighter(name, gender, lethality, spMove) {
+  this.name = name;
+  this.gender = gender;
+  this.lethality = lethality;
+  this.spMove = spMove;
+
+  this.tellSpecialMove = function () {
+    console.log(`this is my ${this.spMove}`);
+  }
+  this.tellLethality = function () {
+    console.log(`this is my ${this.lethality}`);
+  }
+  this.yell = function () {
+    console.log(`ha ha ha! come ONNNNNN`);
+  }
+}
+
+let fighter1 = new MakeFighter('yokho', 'M', 8, 'punch');
+let fighter2 = new MakeFighter('zolo', 'F', 9.2, 'kick');
