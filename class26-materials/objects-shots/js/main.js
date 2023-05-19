78d9f7b7b1457b708c8af973edbc5cf03fa39d3e
owaisbukhari00 <owaisbukhari@outlook.com>
2023-05-20 00:49:33 +0500

    handle spaces in input field

--- class26-materials/objects-shots/js/main.js
+++ class26-materials/objects-shots/js/main.js
@@ -1,18 +1,29 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 
-document.querySelector('button').addEventListener('click', fetchRecipe);
+document.querySelector('button').addEventListener('click', getFetch);
 
-function fetchRecipe(){
-  let cocktailName = document.querySelector('input').value;
+function getFetch(){
+  let inputField = document.querySelector('input');
+  let choiceArray = inputField.value.split(" ");
   
-  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`)
+  console.log(choiceArray);
+  
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${choiceArray[0]}`)
   .then(res => res.json())
   .then(data => {
-    console.log(data.drinks[0]);
+    console.log(data);
 
-    document.querySelector('h2').innerText = data.drinks[0].strDrink;
-    document.querySelector('h3').innerText = data.drinks[0].strInstructions;
-    document.querySelector('img').src = data.drinks[0].strDrinkThumb;
+    let choiceString = choiceArray.join(" ");
+    console.log(choiceString);
+
+    for (let i = 0; i < data.drinks.length; i++) {
+      if(data.drinks[i].strDrink.match(choiceString))
+      {
+        document.querySelector('h2').innerText = data.drinks[i].strDrink;
+        document.querySelector('h3').innerText = data.drinks[i].strInstructions;
+        document.querySelector('img').src = data.drinks[i].strDrinkThumb;
+      }              
+    }    
   })
   .catch(err => {
     console.log(`error ${err}`);
