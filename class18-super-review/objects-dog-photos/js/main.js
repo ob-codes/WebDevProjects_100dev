0130f8700de9a392ec77085fa1e31d9a99d9afaf
owaisbukhari00 <owaisbukhari@outlook.com>
2023-05-20 15:29:49 +0500

    class review dog ceo api

---
+++ class18-super-review/objects-dog-photos/js/main.js
@@ -0,0 +1,17 @@
+//Get a dog photo from the dog.ceo api and place the photo in the DOM
+getFetch();
+
+function getFetch() {
+  const url = "https://dog.ceo/api/breeds/image/random";
+
+  fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+
+    document.querySelector("img").src = data.message;
+  })
+  .catch(err => {
+    console.log(`err: ${err}`)
+  });
+}
