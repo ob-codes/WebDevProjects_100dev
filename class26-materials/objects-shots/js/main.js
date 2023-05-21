d9f1d07d73bf61de9469fbe3a2e0dd6c2da59bd9
owaisbukhari00 <owaisbukhari@outlook.com>
2023-05-21 16:48:51 +0500

    made input case insensitive

--- class26-materials/objects-shots/js/main.js
+++ class26-materials/objects-shots/js/main.js
@@ -5,19 +5,19 @@ document.querySelector('button').addEventListener('click', getFetch);
 function getFetch(){
   let inputField = document.querySelector('input');
   let choiceArray = inputField.value.split(" ");
-  
+
   console.log(choiceArray);
   
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${choiceArray[0]}`)
   .then(res => res.json())
   .then(data => {
-    console.log(data);
-
+    console.log(data);    
+    
     let choiceString = choiceArray.join(" ");
     console.log(choiceString);
 
     for (let i = 0; i < data.drinks.length; i++) {
-      if(data.drinks[i].strDrink.match(choiceString))
+      if(data.drinks[i].strDrink.toLowerCase().match(choiceString.toLowerCase()))
       {
         document.querySelector('h2').innerText = data.drinks[i].strDrink;
         document.querySelector('h3').innerText = data.drinks[i].strInstructions;
