c85401833e765e83ab96d6a1a2589cbc413236a4
owaisbukhari00 <owaisbukhari@outlook.com>
2023-06-07 23:18:42 +0500

    populated DOM with retrieved data

--- class21-materials/objects-shots/js/main.js
+++ class21-materials/objects-shots/js/main.js
@@ -11,6 +11,9 @@ function getFetch() {
       console.log(data);
 
       document.querySelector("h2").innerText = data.drinks[0].strDrink;
+      document.querySelector("img").src = data.drinks[0].strDrinkThumb;
+      document.querySelector("img").style.width = "300px";
+      document.querySelector("h3").innerText = data.drinks[0].strInstructions;
     })
     .catch(err => {
       console.log(`err: ${err}`);
