bb64aff8eec3bd86df3dbcc906112392f89ecd71
owaisbukhari00 <owaisbukhari@outlook.com>
2023-07-27 00:32:04 +0500

    added setter and getter functions

--- class32-materials/objects-practice/js/main.js
+++ class32-materials/objects-practice/js/main.js
@@ -1,24 +1,30 @@
 // Come up with with a parent class
 // Extend that parent class into two children
 // Use Encapsulation, Abstraction, Inheritance, and Polymorphism 
-
+/*
 class Contractor{
   constructor(totalHours, hourlyRate, hourlyExpense) {
     this.totalHours = totalHours;
-    this.hourlyRate = hourlyRate;
-    this.hourlyExpense = hourlyExpense;  
+    this._hourlyRate = hourlyRate;
+    this._hourlyExpense = hourlyExpense;  
   }
   tellBill(){
     let bill = this.totalHours * this.hourlyRate;
     return bill;
   }
-  tellExpense(){
-    let expense = this.totalHours * this.hourlyExpense;   
-    return expense;
+  get tellExpense(){
+    let _expense = this.totalHours * this.hourlyExpense;   
+    return _expense;
   }
-  tellProfit(){
-    let profit = this.tellBill() - this.tellExpense();
-    return profit;
+  get tellProfit(){
+    let _profit = this.tellBill() - this.tellExpense();
+    return _profit;
+  }
+  set setRole(newRole){
+    this._role = newRole;
+  }
+  get getRole(){
+    return this._role;
   }
 }
 
@@ -54,3 +60,52 @@ class Backend extends Contractor{
 
 let fe = new Frontend(8);
 let be = new Backend(12);
+*/
+class Contractor{
+  constructor(name, role){
+    this._name = name;
+    this._role = role;
+  }
+  get name(){
+    return this._name;
+  }
+  get role(){
+    return this._role;
+  }
+  set role(newRole){
+    this._role = newRole;
+  }
+  sayHello(){
+    console.log(`Hi I am ${this._name} and I'm ${this._role}`)
+  }
+}
+
+class Frontend extends Contractor{
+  constructor(name, role, tech){
+    super(name, role);
+    this._tech = tech;
+  }
+  get tech(){
+    return this._tech;
+  }
+  sayHello(){
+    console.log(`Hi I am ${this._name} working as a FrontEnd engineer`)
+  }
+}
+
+class Backend extends Contractor{
+  constructor(name, role, tech){
+    super(name, role);
+    this._tech = tech;
+  }
+  get tech(){
+    return this._tech;
+  }
+  sayHello(){
+    console.log(`Hi I am ${this._name} working as a BackEnd engineer`)
+  }
+}
+
+let cont = new Contractor("Moshi", "Cont");
+let fee = new Frontend("Moshi", "FE", "React");
+let bee = new Backend("Moshi", "BE", "Node");
