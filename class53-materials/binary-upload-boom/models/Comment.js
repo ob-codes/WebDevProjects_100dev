0669fe0c9dedb104aeb6ee55ea5ab6fbdf93a0b8
owaisbukhari00 <owaisbukhari@outlook.com>
2024-01-14 23:47:28 +0500

    created comment model to store comments on posts

---
+++ class53-materials/binary-upload-boom/models/Comment.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+  desc: {
+    type: String,
+    required: true,
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  postId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+  },
+  likes: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
