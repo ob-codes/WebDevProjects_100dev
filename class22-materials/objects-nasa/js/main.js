ff12972c4db6c2275ff1740b887a1075afd3714f
owaisbukhari00 <owaisbukhari@outlook.com>
2024-01-24 14:18:12 +0500

    reviewed Class-22: added fetch api call

---
+++ class22-materials/objects-nasa/js/main.js
@@ -0,0 +1,22 @@
+//The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+document.querySelector("button").addEventListener('click', getPic);
+
+function getPic(params) {
+  const date = document.querySelector('input').value;
+  const url = `https://api.nasa.gov/planetary/apod?api_key=ykOd0vTZ7OnoVeAmAMePt2qkFzC09RHn40R5cvnG&date=${date}`
+  
+  fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+    
+    document.querySelector('h2').innerText = data.title;
+    document.querySelector('img').src = data.hdurl;
+    document.querySelector('img').style.width = '300px';
+
+    document.querySelector('p').innerText = data.explanation;
+  })
+  .catch(err => {
+    console.log(`err: ${err}`);
+  });
+}
