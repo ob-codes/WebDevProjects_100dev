eb02d49a0a24d46f426e026518df4b1556fbce07
owaisbukhari00 <owaisbukhari@outlook.com>
2024-03-02 20:47:03 +0500

    added button to the header component

--- react-app-traversy-media/task-tracker/src/components/Header.js
+++ react-app-traversy-media/task-tracker/src/components/Header.js
@@ -1,13 +1,31 @@
+import PropTypes from 'prop-types';
+import Button from './Button';
+
 const Header = ({title}) => {
+  const onClick = (e) => {
+    console.log(`button-${title} clicked`)
+  }
+  
   return (
-    <header>
+    <header className='header'>
       <h1>{title}</h1>
+      <Button title='Add' color='green' onClick={onClick}></Button>      
     </header>
   );
 }
 
+// CSS in JS
+// const headingStyle = {
+//   color: 'yellow', 
+//   backgroundColor: 'black',
+// }
+
 Header.defaultProps = {
   title: 'Task Tracker',
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
 }
 
 export default Header;
